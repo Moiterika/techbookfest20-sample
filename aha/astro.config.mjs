a1cf778a9084b03fb44006d833aaefdba9e1cdda
saeki-hisafumi <saeki-hisafumi@moiterika.jp>
2026-03-21 20:42:30 +0900

    htmx追加他
    - bun fmt/bun lintを実装

--- aha/astro.config.mjs
+++ aha/astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+
+import node from "@astrojs/node";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
   adapter: node({
-    mode: 'standalone',
+    mode: "standalone",
   }),
   server: {
     host: true,
